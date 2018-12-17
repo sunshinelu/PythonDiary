70cf1cb38f65c115739c980cd5827a2a0cacd48d
sunlu <sunlu900326@yeah.net>
2018-12-17 15:46:44 +0800

    模型评价

--- machineLearning/evaluating/confusion_matrix_Demo1.py
+++ machineLearning/evaluating/confusion_matrix_Demo1.py
@@ -9,6 +9,18 @@ y_pred = ["ant", "ant", "cat", "cat", "ant", "cat"]
 
 label = list(set(y_true))
 
+n = len(label)
+lst = []
+
+for i in range(n):
+    # lst = lst + ["lable_" + str(i)] # 方法一
+    lst.append("lable_" + str(i))  # 方法二
+
+print lst
+# ['lable_0', 'lable_1', 'lable_2']
+
+lst = list(lst)
+
 matrix = confusion_matrix(y_true, y_pred, labels=label)
 matrix_df = pd.DataFrame(matrix, columns=label, index=label)
 
@@ -18,4 +30,14 @@ print matrix_df
 ant     2     0    0
 bird    0     0    1
 cat     1     0    2
+"""
+
+matrix_df2 = pd.DataFrame(matrix, columns=lst, index=lst)
+print matrix_df2
+"""
+         lable_0  lable_1  lable_2
+lable_0        2        0        0
+lable_1        0        0        1
+lable_2        1        0        2
+
 """
