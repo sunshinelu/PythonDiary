13d933032ed09b7628604456e1a52d3ca5de3792
sunlu <sunlu900326@yeah.net>
2019-06-24 22:41:17 +0800

    pyspark mysql

---
+++ Spark/DataBase/MySQL/pyspark_mysql_demo2.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from pyspark.sql import SparkSession
+
+spark = SparkSession\
+        .builder\
+        .appName("Flark - Flask on Spark")\
+        .getOrCreate()
+
+ds1 = spark.read.jdbc(url="jdbc:mysql://localhost:3306/data_mining_db?user=root&password=root",
+                      table="ml_info_item",
+                      properties={"driver": 'com.mysql.jdbc.Driver'})
+
+ds1.show(5)
